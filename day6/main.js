835a97debc89eab474a2bc25aa214899ead3f916
Jiangtao <jiangtao19993@gmail.com>
2017-08-27 21:38:55 +0800

    day6 prepared

---
+++ day6/main.js
@@ -0,0 +1,72 @@
+/**
+ * Created by huang on 2017/8/20.
+ */
+
+var loadLevel = function (game, n) {
+	var blocks = []
+	var n = n - 1
+	var level = levels[n]
+	for (var i = 0; i < level.length; i++){
+		var p = level[i]
+		var b = Block(game, p)
+		blocks.push(b)
+	}
+	return blocks
+	
+	
+}
+var enableDebugMode = function (game, enable) {
+	if (!enable){
+		return
+	}
+	window.paused = false
+	window.addEventListener('keydown',function (event) {
+		var k = event.key
+		if(k == 'q'){
+			//为了debug
+			window.paused = !window.paused
+		}else if ('1234567'.includes(k)){
+			//为了debug，临时加的关卡
+			log("1213", k)
+			blocks = loadLevel(game, Number(k))
+		}
+	})
+	// 控制速度
+	document.querySelector("#id-input-speed").addEventListener("input", function (event) {
+		var input = event.target
+		window.fps = Number(input.value)
+	})
+}
+
+var _main = function() {
+	
+	var images = {
+		ball: 'ball.png',
+		block: 'block.png',
+		paddle: 'paddle.png',
+	}
+	
+	var game = Game(30, images,  function (g) {
+		var s = Scene(g)
+		g.runWithScene(s)
+		
+		// game.update = function() {
+		// 	if (window.paused){
+		// 		return
+		// 	}
+		// 	// s.update
+		// 	scene.update()
+		//
+		// }
+		//
+		// game.draw = function() {
+		// 	scene.draw()
+		// }
+		
+	})
+	enableDebugMode(game,true)
+	
+	
+	
+}
+_main()
---
+++ day6/scene.js
@@ -0,0 +1,93 @@
+var Scene = function (game) {
+	var s = {
+		game: game,
+	}
+	//初始化数据
+	var paddle = Paddle(game)
+	var ball = Ball(game)
+	var score = 0
+	var blocks = loadLevel(game, 1)
+	
+	game.registerAction('a', function(){
+		paddle.moveLeft()
+	})
+	game.registerAction('d', function(){
+		paddle.moveRight()
+	})
+	game.registerAction('f', function(){
+		ball.fire()
+	})
+	
+	s.draw = function () {
+		// 背景图
+		game.context.fillStyle = "#554"
+		game.context.fillRect(0, 0,  400, 300)
+		
+		game.drawImage(paddle)
+		game.drawImage(ball)
+		// draw block
+		for(var i = 0; i < blocks.length; i++) {
+			var block = blocks[i]
+			if (block.alive) {
+				game.drawImage(block)
+			}
+		}
+		game.context.fillText('分数： '+ score, 10, 280)
+	}
+	
+	s.update = function () {
+		if(window.paused){
+			return
+		}
+		ball.move()
+		if(paddle.collide(ball)){
+			ball.speedY *= -1
+		}
+		// 判断游戏结束
+		if (ball.y > paddle.y) {
+			// 跳转到 游戏结束 的场景
+			var end = SceneEnd(game)
+			game.replaceScene(end)
+		}
+		//判断ball和block相撞
+		for(var i = 0; i < blocks.length; i++){
+			var block = blocks[i]
+			if(block.collide(ball)){
+				log("block相撞")
+				block.kill()
+				ball.反弹()
+				// 更新分数
+				score += 100
+			}
+		}
+	}
+	
+	// mouse event
+	var enableDrag = false
+	game.canvas.addEventListener('mousedown', function (event) {
+		var x = event.offsetX
+		var y = event.offsetY
+		log(x, y, 'down')
+		if(ball.hasPoint(x, y)){
+			log("点中")
+			enableDrag = true
+		}
+	})
+	game.canvas.addEventListener('mousemove', function (event) {
+		var x = event.offsetX
+		var y = event.offsetY
+		if(enableDrag){
+			ball.x = x
+			ball.y = y
+		}
+	})
+	
+	game.canvas.addEventListener('mouseup', function (event) {
+		var x = event.offsetX
+		var y = event.offsetY
+		// log(x, y, 'up')
+		enableDrag = false
+	})
+	
+	return s
+}
